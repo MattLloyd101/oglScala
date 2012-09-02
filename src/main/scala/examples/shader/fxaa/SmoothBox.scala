19256884ef417df8307b0ec544b09f783362f237
Matthew Lloyd <Matt.Lloyd101@gmail.com>
2012-09-03 00:25:06 +0100

    Testing

--- src/main/scala/examples/shader/fxaa/SmoothBox.scala
+++ src/main/scala/examples/shader/fxaa/SmoothBox.scala
@@ -2,18 +2,9 @@ package examples.shader.fxaa
 
 import com.mattlloyd.opengl.shader._
 import org.lwjgl.opengl.GL11
-import shader.{FxaaShader}
+import shader.FxaaShader
 import com.mattlloyd.opengl.VBO.VBOObject
 import com.mattlloyd.opengl.{Renderable, Updatable, Initable}
-import scala.math._
-
-/**
- * Created with IntelliJ IDEA.
- * User: Matt
- * Date: 29/07/12
- * Time: 14:56
- * To change this template use File | Settings | File Templates.
- */
 
 class SmoothBox extends Renderable[Unit] with Initable[Unit] with Updatable[Unit] with Shaded with VBOObject {
 
@@ -22,7 +13,8 @@ class SmoothBox extends Renderable[Unit] with Initable[Unit] with Updatable[Unit
     val pos = shaderProgram.pos
     val rot = shaderProgram.rot
 
-    def init = {
+    // test commit.
+    def init {
 
     }
 
@@ -31,12 +23,12 @@ class SmoothBox extends Renderable[Unit] with Initable[Unit] with Updatable[Unit
     }
 
     def render {
-        GL11.glLoadIdentity
+        GL11.glLoadIdentity()
         GL11.glBegin(GL11.GL_QUADS)
         GL11.glVertex3f(-1.0f, 1.0f, 0.0f)
         GL11.glVertex3f(1.0f, 1.0f, 0.0f)
         GL11.glVertex3f(1.0f, -1.0f, 0.0f)
         GL11.glVertex3f(-1.0f, -1.0f, 0.0f)
-        GL11.glEnd
+        GL11.glEnd()
     }
 }
